4cc60d7e13214f80db422f9c0df66f8d29a52eca
544146 <wee.tommy99@gmail.com>
2023-03-20 03:50:20 +0000

    Run pre-commit on entire project

--- scripts/sd-webui-aspect-ratio-helper.py
+++ scripts/sd-webui-aspect-ratio-helper.py
@@ -2,54 +2,56 @@ import contextlib
 from functools import partial
 
 import gradio as gr
-from modules import scripts, shared, script_callbacks
+from modules import script_callbacks
+from modules import scripts
+from modules import shared
 from modules.shared import opts
 
 _MIN_DIMENSION = 64
 _MAX_DIMENSION = 2048
-_EXTENSION_NAME = "Aspect Ratio Helper"
+_EXTENSION_NAME = 'Aspect Ratio Helper'
 
 
 def on_ui_settings():
-    section = "aspect_ratio_helper", _EXTENSION_NAME
+    section = 'aspect_ratio_helper', _EXTENSION_NAME
     shared.opts.add_option(
-        key="arh_expand_by_default",
+        key='arh_expand_by_default',
         info=shared.OptionInfo(
             default=False,
-            label="Expand by default",
-            section=section
+            label='Expand by default',
+            section=section,
         ),
     )
     shared.opts.add_option(
-        key="arh_show_max_width_or_height",
+        key='arh_show_max_width_or_height',
         info=shared.OptionInfo(
             default=True,
-            label="Show maximum width or height button",
-            section=section
+            label='Show maximum width or height button',
+            section=section,
         ),
     )
     shared.opts.add_option(
-        key="arh_max_width_or_height",
+        key='arh_max_width_or_height',
         info=shared.OptionInfo(
             default=1024,
-            label="Maximum width or height default",
-            section=section
+            label='Maximum width or height default',
+            section=section,
         ),
     )
     shared.opts.add_option(
-        key="arh_show_predefined_percentages",
+        key='arh_show_predefined_percentages',
         info=shared.OptionInfo(
             default=True,
-            label="Show percentage buttons",
-            section=section
+            label='Show percentage buttons',
+            section=section,
         ),
     )
     shared.opts.add_option(
-        key="arh_predefined_percentages",
+        key='arh_predefined_percentages',
         info=shared.OptionInfo(
-            default="25, 50, 75, 125, 150, 175, 200",
-            label="Percentage buttons (75, 125, 150)",
-            section=section
+            default='25, 50, 75, 125, 150, 175, 200',
+            label='Percentage buttons (75, 125, 150)',
+            section=section,
         ),
     )
 
@@ -92,12 +94,17 @@ class AspectRatioStepScript(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
-        if not any([opts.arh_show_max_width_or_height, opts.arh_show_predefined_percentages]):
+        if not any([
+            opts.arh_show_max_width_or_height,
+            opts.arh_show_predefined_percentages,
+        ]):
             return  # return early as no 'show' options enabled
 
-        with (gr.Group(),
-              gr.Accordion(_EXTENSION_NAME, open=opts.arh_expand_by_default),
-              contextlib.suppress(AttributeError)):
+        with (
+            gr.Group(),
+            gr.Accordion(_EXTENSION_NAME, open=opts.arh_expand_by_default),
+            contextlib.suppress(AttributeError),
+        ):
             if is_img2img:
                 inputs = outputs = [self.i2i_w, self.i2i_h]
             else:
@@ -110,9 +117,9 @@ class AspectRatioStepScript(scripts.Script):
                         maximum=2048,
                         step=16,
                         default=opts.arh_max_width_or_height,
-                        label="Maximum width or height (whichever is higher)"
+                        label='Maximum width or height (whichever is higher)',
                     )
-                    gr.Button(value="Scale to maximum width or height").click(
+                    gr.Button(value='Scale to maximum width or height').click(
                         fn=_scale_dimensions_to_max_dimension,
                         inputs=[*inputs, max_dimension],
                         outputs=outputs,
@@ -120,24 +127,29 @@ class AspectRatioStepScript(scripts.Script):
 
             if opts.arh_show_predefined_percentages:
                 with gr.Column(variant='panel'), gr.Row(variant='compact'):
-                    percentages = [int(x) for x in opts.arh_predefined_percentages.split(',')]
+                    pps = opts.arh_predefined_percentages
+                    percentages = [
+                        int(x) for x in pps.split(',')
+                    ]
                     for percentage in percentages:
-                        gr.Button(value=f"{str(percentage)}%").click(
-                            fn=partial(_scale_by_percentage, pct=percentage / 100),
+                        gr.Button(value=f'{str(percentage)}%').click(
+                            fn=partial(
+                                _scale_by_percentage, pct=percentage / 100,
+                            ),
                             inputs=inputs,
                             outputs=outputs,
                         )
 
     def after_component(self, component, **kwargs):
-        element_id = kwargs.get("elem_id")
+        element_id = kwargs.get('elem_id')
 
-        if element_id == "txt2img_width":
+        if element_id == 'txt2img_width':
             self.t2i_w = component
-        elif element_id == "txt2img_height":
+        elif element_id == 'txt2img_height':
             self.t2i_h = component
-        elif element_id == "img2img_width":
+        elif element_id == 'img2img_width':
             self.i2i_w = component
-        elif element_id == "img2img_height":
+        elif element_id == 'img2img_height':
             self.i2i_h = component
 
 
